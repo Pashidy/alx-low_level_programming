3ea7840c2582aabb62653101ec26b59f25d847d1
pashidy <pash@example.com>
2023-02-16 13:34:46 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main - An interactive program using puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle")
+return (0);
+}
