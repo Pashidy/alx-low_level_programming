72f3c4d4a14f3f702ce25e7cb6c522f10f3b91cf
pashidy <pash@example.com>
2023-02-21 05:53:34 -0800

    9p_p_p

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 48; p < 58; p++)
+	{
+		putchar(p);
+
+		if (p != 57)
+		{
+			putchar(',');
+
+			putchar(' ');
+		}
+	putchar('\n');
+
+	return (0);
+
+	}
+}
+
