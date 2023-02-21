374663ea842a78600ec7c05cc259cac64f10f1fb
pashidy <pash@example.com>
2023-02-20 19:35:34 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+/**
+ * main - main prints lower case alphabets
+ *
+ * return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char li;
 
+	for (li = 'a'; li <= 'z'; li++)
+	{
+		putchar(li);
+	}
+	putchar('\n');
+	return (0);
+}
