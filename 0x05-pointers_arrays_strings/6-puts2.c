bb8bc8324bab455ec0d1dd9f6ba28653b5b93361
pashidy <pash@example.com>
2023-03-01 13:33:53 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (; str[i] != '\0'; i++)
 	{
-	_putchar(str[i]);
+		if (( i % 2 ) == 0)
+		
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
