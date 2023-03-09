b7a8359c7ef7d94f3e4790b96e3491985ebd97c5
pashidy <pash@example.com>
2023-03-09 12:33:55 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,16 +7,36 @@
  *
  * Return: 1 if the number is prime, 0 otherwise
  */
+
 int is_prime_number(int n)
 {
-	int i;
+	int i = 3;
 
-	if (n <= 1)
-		return (0);
-	for (i = 2; i * i <= n; i++)
+	if (n < 2)
 	{
-		if (n % i == 0)
-			return (0);
+		return (0);
 	}
+
+	if (n == 2)
+	{
+		return (1);
+	}
+
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+
+	while (i * i <= n)
+	{
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	i += 2;
+	}
+
 	return (1);
 }
+
