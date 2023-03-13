14412dbb3dedf3364f1f7c1164415b48e6e7cbbe
pashidy <pash@example.com>
2023-03-12 18:14:59 -0700

    my putchar

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
