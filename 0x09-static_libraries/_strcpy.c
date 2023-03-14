e7cb9577a0aa3c77d0e921c4d861a4486867fcf1
pashidy <pash@example.com>
2023-03-14 03:01:03 -0700

    strcpy

---
+++ 0x09-static_libraries/_strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte,
+ * to the buffer pointed to by dest
+ *
+ * @dest: buffer to copy to
+ * @src: buffer to copy from
+ *
+ * Return: pointer to dest.
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	char *p = dest;
+
+	while (*dest++ = *src++)
+
+	return (p);
+}
