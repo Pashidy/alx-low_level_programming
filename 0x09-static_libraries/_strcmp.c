b88fe3664d64fc6a65934847f154cacf9e30cee3
pashidy <pash@example.com>
2023-03-15 04:48:07 -0700

    strcmp

--- 0x09-static_libraries/_strcmp.c
+++ 0x09-static_libraries/_strcmp.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include "main.h"
 
 /**
  * _strcmp - Compare two strings.
@@ -9,13 +10,19 @@
  * or greater than zero if s1 is found,
  * respectively, to be less than, to match, or be greater than s2.
  */
-int _strcmp(const char *s1, const char *s2)
+
+int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+	if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
 	}
 
-	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+	if (s1[i] == s2[i])
+	return (0);
+
+	return (s1[i] < s2[i] ? -1 : 1);
 }
