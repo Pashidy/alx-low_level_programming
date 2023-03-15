2bdb94798e5ae7ad0f9e217f7b75cbf50e80e50f
pashidy <pash@example.com>
2023-03-15 05:42:12 -0700

    strchr

--- 0x09-static_libraries/_strchr.c
+++ 0x09-static_libraries/_strchr.c
@@ -2,27 +2,23 @@
 #include "main.h"
 
 /**
- * _strchr - Locate the first occurrence of a character in a string.
- * @s: The string to search.
- * @c: The character to search for.
+ * _strchr - locates a character in a string
+ * @s: the string to search in
+ * @c: the character to find
  *
- * Return: A pointer to the first occurrence of the character in the string,
- *         or NULL if the character is not found.
+ * Return: a pointer to the first occurrence of
+ * the character c in the string s,
+ * or NULL if the character is not found
  */
-char *_strchr(const char *s, int c)
+char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
 		if (*s == c)
-
-		return ((char *) s);
-
+			return (s);
 		s++;
 	}
-
 	if (*s == c)
-
-	return ((char *) s);
-
+		return (s);
 	return (NULL);
 }
