963f65e35a7f36e05f35d0447610dcd1c58b14da
pashidy <pash@example.com>
2023-03-19 05:05:09 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,17 @@
  *
  * Description: Prints the name of the program, including the path,
  *              followed by a new line.
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the strings which
+ * are those arguments
  *
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
+	if (argc >= 1)
+	{
 	printf("%s\n", argv[0]);
+	}
 	return (0);
 }
