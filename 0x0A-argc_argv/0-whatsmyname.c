c051d68e59110a4009c4d2c9751eeda6e9804b29
pashidy <pash@example.com>
2023-03-19 04:19:54 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the program, including the path,
+ *              followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
