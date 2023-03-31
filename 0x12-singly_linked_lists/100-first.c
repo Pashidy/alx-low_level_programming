7fbdd5702a0ee4aa179a1f90934a8bda056731c2
pashidy <pash@example.com>
2023-03-30 17:46:11 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before_main - prints the required text before the main function is executed
+ *
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
