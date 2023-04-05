9f5c39d90530289709088d2c82ce2b60a5fd4bde
pashidy <pash@example.com>
2023-04-05 16:19:50 -0700

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,13 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *current;
 
-	while (head)
+	while (head != NULL)
 	{
-	temp = head->next;
-	free(head);
-	head = temp;
+		current = head;
+		head = head->next;
+		free(current);
 	}
+
 }
