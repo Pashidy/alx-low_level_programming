d93af8b9a5fe9ade6bf5ea56fee4a4b258e8ddae
Pashidy <eldafrancis@yahoo.com>
2023-12-11 15:03:39 -0800

    Task2

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - module of two integers
+ * @a: first int
+ * @b: second int
+ * Return: module of both integers
+ */
+int mod(int a, int b)
+{       
+        return (a % b);
+}
